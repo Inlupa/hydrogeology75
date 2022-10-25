00c78798c6f7cc8be56a2a85a14b13c23c4b3e77
Inlupa <anton199823@gmail.com>
2022-10-25 16:19:57 +0300

    Новое приложения для регистрации участников конференции

--- hydro/hydro/urls.py
+++ hydro/hydro/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('mainpage.urls')),
     path('admin/', admin.site.urls),
+    path('', include('registration.urls')),
 ]
 
 if settings.DEBUG:
